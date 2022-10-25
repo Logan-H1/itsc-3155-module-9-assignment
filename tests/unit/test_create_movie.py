8e05fea506cea6025d140c0f023f979d9e60489e
Logan Hamilton <loganhamilton622@gmail.com>
2022-10-24 21:47:05 -0400

    fixing part 2 tests

--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -4,46 +4,15 @@ from app import app, movie_repository
 import pytest
 from src.models.movie import Movie
 
-@pytest.fixture()
-def test_app():
-  return app.test_client()
 
-def test_movie_created(test_app):
-    response = test_app.post('/movies', data={
-        "movie_name": "The Dark Knight",
-        "director_name": "Christopher Nolan",
-        "movie_rating": 5
-    })
 
-    test_movie = Movie("The Dark Knight", "Christopher Nolan", 5)
+def test_movie_created():
+    movie_repository.create_movie("The Dark Knight", "Christopher Nolan", 5)
     all_movies = movie_repository.get_all_movies()
-    print(test_movie.__dict__)
-
+    test_movie = Movie("The Dark Knight", "Christopher Nolan", 5)
     same_movie = False
-
-    
     for movie in all_movies:
         if movie.__dict__ == test_movie.__dict__:
             same_movie = True
     
     assert same_movie
-
-def test_movie_created_bad(test_app):
-    response = test_app.post('/movies', data={
-        "movie_name": "The Dark Knight",
-        "director_name": "Christopher Nolan",
-        "movie_rating": 5
-    })
-
-    test_movie = Movie("The Bright Knight", "Logan Hamilton", 5)
-    all_movies = movie_repository.get_all_movies()
-    print(test_movie.__dict__)
-
-    same_movie = False
-
-    
-    for movie in all_movies:
-        if movie.__dict__ == test_movie.__dict__:
-            same_movie = True
-    
-    assert not same_movie
