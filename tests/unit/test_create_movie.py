51b984737b9de3bdcf6b4bd3f545012e76e930fa
Logan Hamilton <loganhamilton622@gmail.com>
2022-10-24 21:55:42 -0400

    fixing part 2 tests #2

--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -1,4 +1,4 @@
-# TODO: Feature 2
+# TODO: Feature 2 Done by Logan Hamilton
 from src.repositories.movie_repository import get_movie_repository
 from app import app, movie_repository
 import pytest
@@ -16,3 +16,14 @@ def test_movie_created():
             same_movie = True
     
     assert same_movie
+
+def test_movie_created():
+    movie_repository.create_movie("The Dark Knight", "Christopher Nolan", 5)
+    all_movies = movie_repository.get_all_movies()
+    test_movie = Movie("Inception", "Christopher Nolan", 5)
+    same_movie = False
+    for movie in all_movies:
+        if movie.__dict__ == test_movie.__dict__:
+            same_movie = True
+    
+    assert not same_movie
