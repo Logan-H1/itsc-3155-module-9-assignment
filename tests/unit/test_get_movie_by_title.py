3967b09a31838f92ac103d61cffd7e33f7b46a7c
SorenWilson164 <113070685+SorenWilson164@users.noreply.github.com>
2022-10-25 22:37:09 -0400

    search e2e and unit test
    search tests complete

--- tests/unit/test_get_movie_by_title.py
+++ tests/unit/test_get_movie_by_title.py
@@ -1,1 +1,43 @@
-# TODO: Feature 3
+# TODO: Feature 3 Done by Soren Wilson
+from src.models.movie import Movie
+from app import movie_repository
+from src.repositories.movie_repository import get_movie_repository
+
+import pytest
+
+
+def test_get_movie_by_title():
+    check_rating = "5"
+    movie_repository.create_movie("1917", "Sam Mendes", "5")
+    by_title = movie_repository.get_movie_by_title("1917")
+    
+    same_rating = False
+
+    if(by_title.rating == check_rating):
+        same_rating = True
+
+    assert same_rating
+
+def test_get_movie_by_title():
+    check_rating = "5"
+    movie_repository.create_movie("1917", "Sam Mendes", "5")
+    by_title = movie_repository.get_movie_by_title("Hacksaw Ridge")
+    
+    same_rating = False
+    
+    if(by_title.rating == check_rating):
+        same_rating = True
+
+    assert not same_rating
+
+def test_get_movie_by_title():
+    check_rating = "5"
+    movie_repository.create_movie("1917", "Sam Mendes", "5")
+    by_title = movie_repository.get_movie_by_title("1917")
+    
+    same_rating = False
+    
+    if(by_title.rating == "4"):
+        same_rating = True
+
+    assert not same_rating
